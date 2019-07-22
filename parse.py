adabcb2e3b7a248b8020469149c039ada094786b
Kai <kai.pinckard@gmail.com>
2019-07-22 16:45:55 -0700

    fix the code, so that now all the subdirectories' files are correctly scanned as well. Also refactor the code into functions for easier use.

--- parse.py
+++ parse.py
@@ -3,25 +3,49 @@ from cd import Cd
 import os
 
 """
-The Cd context manager assumes that we will only be navigating
-to more specific directories, so it needs to start outside of each 
-of the folders. 
+Returns a list of dictionaries, where each dictionary contains all the key 
+value pairs of a word element.
 """
-os.chdir('..')
-with Cd('word_sense_disambigation_corpora'):
-    with Cd('masc/written/blog/'):
-        for f in os.listdir():
-            xmldoc = minidom.parse(f)
-            itemlist = xmldoc.getElementsByTagName('word')
+def parseXmlFile(f):
+    print(f)
+    xmldoc = minidom.parse(f)
+    itemlist = xmldoc.getElementsByTagName('word')
 
-            wordList = []
-            for s in itemlist:
-                for key in s.attributes.keys():
-                    dct = {}
-                    dct[key] = s.attributes[key].value
-                    wordList.append(dct)
+    wordList = []
+    for word in itemlist:
+        for key in word.attributes.keys():
+            dct = {}
+            dct[key] = word.attributes[key].value
+            wordList.append(dct)
+    return wordList
+
+"""
+Returns a list of dictionaries, containing all the word data in all the xml files
+in the specified directory and all of its sub directories.
+"""
+def parseDirectory(directory):
+    data = []
+    for root, dirs, files in os.walk(directory_to_walk):
+        with Cd(root):
+            print(os.getcwd())
+            for f in files:
+                if f.endswith(".xml"):
+                    data.extend(parseXmlFile(f))
+    return data
+
+
+
+if __name__ == "__main__":
+    os.chdir('..')
+    directory_to_walk = os.path.join(os.getcwd(), 'word_sense_disambigation_corpora')
+    data = parseDirectory(directory_to_walk)
+
+
+    for word in data:
+        line = ""
+        for key in word.keys():
+            line += key + ": " + word[key] + " "
+        print(line) 
                 
-            for w in wordList:
-                for key in w.keys():
-                    print(w[key])
-                print()
+        
+        
