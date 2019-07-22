95b31919a7bf3dcd80531898949a15bcafff2a8c
Kai David Pinckard <kaidpinck@c097h152.acad.reed.edu>
2019-07-22 11:56:33 -0700

    add a context manager to control the current folder.

--- parse.py
+++ parse.py
@@ -1,8 +1,26 @@
 from xml.dom import minidom
+from cd import Cd
+import os
 
-xmldoc = minidom.parse('word_sense_disambigation_corpora/masc/written/blog/Acephalous-Cant-believe.xml')
-itemlist = xmldoc.getElementsByTagName('word')
-print(len(itemlist))
-print(itemlist[0].attributes['text'].value)
-for s in itemlist:
-    print(s.attributes['text'].value)
+"""
+The Cd context manager assumes that we will only be navigating
+to more specific directories, so it needs to start outside of each 
+of the folders. 
+"""
+os.chdir('..')
+with Cd('word_sense_disambigation_corpora'):
+    with Cd('masc/written/blog/'):
+        files = os.listdir()
+        xmldoc = minidom.parse(files[0])
+        itemlist = xmldoc.getElementsByTagName('word')
+        print(len(itemlist))
+        print(itemlist[0].attributes['text'].value)
+
+        wordList = []
+        for s in itemlist:
+            dct = {}
+            dct["text"] = s.attributes['text'].value
+            wordList.append(dct)
+        
+        for w in wordList:
+            print(dct["text"])
