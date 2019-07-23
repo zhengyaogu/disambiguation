3da2b674545e6b09f8d58b5a8a3a723314668eec
Kai <kai.pinckard@gmail.com>
2019-07-23 11:59:52 -0700

    add a link to our newly formatted json data.

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 from xml.dom import minidom
 from cd import Cd
 import os
+import json
+import pprint
 
 """
 Returns a list of dictionaries, where each dictionary contains all the key 
@@ -9,15 +11,24 @@ value pairs of a word element.
 def parseXmlFile(f):
     print(f)
     xmldoc = minidom.parse(f)
+    header = xmldoc.getElementsByTagName('SimpleWsdDoc')[0]
+    docname = header.attributes['name'].value
+
     itemlist = xmldoc.getElementsByTagName('word')
 
+    document_dictionary = {}
+    document_dictionary["docname"] = docname
     wordList = []
     for word in itemlist:
+        dct = {}
         for key in word.attributes.keys():
-            dct = {}
+            
             dct[key] = word.attributes[key].value
-            wordList.append(dct)
-    return wordList
+        wordList.append(dct)
+    
+    document_dictionary["doc"]= wordList
+
+    return document_dictionary
 
 """
 Returns a list of dictionaries, containing all the word data in all the xml files
@@ -27,25 +38,17 @@ def parseDirectory(directory):
     data = []
     for root, dirs, files in os.walk(directory_to_walk):
         with Cd(root):
-            print(os.getcwd())
             for f in files:
                 if f.endswith(".xml"):
-                    data.extend(parseXmlFile(f))
+                    data.append(parseXmlFile(f))
     return data
-
-
 
 if __name__ == "__main__":
     os.chdir('..')
     directory_to_walk = os.path.join(os.getcwd(), 'word_sense_disambigation_corpora')
     data = parseDirectory(directory_to_walk)
+    filename = "googledata.json"
+    os.chdir(os.path.join(os.getcwd(), "disambiguation" ))
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
 
-
-    for word in data:
-        line = ""
-        for key in word.keys():
-            line += key + ": " + word[key] + " "
-        print(line) 
-                
-        
-        
