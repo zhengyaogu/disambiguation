13c577b88b9a1359ce7dbba2debf4782caa8505f
zhengyaogu <zhengyao.gu30@gmail.com>
2019-07-25 10:10:06 -0700

    test update

--- test_compare.py
+++ test_compare.py
@@ -4,9 +4,11 @@ from compare import *
 class TestCompare(unittest.TestCase):
 	
 	def test_track(self):
-		sent = ["But", "there", "\'", "##s", "no", "rea", "##son"]
-		tracking = TrackRawSentenceIndices(sent)
-		assert tracking == [0, 1, 2, 2, 3, 4, 4]
+		raw_sent = ["/", "written", "/", "letters", "/", "112C","-", "L014.txt", "June", "26", ",", "1995", "Dear", "Friend", ":"]
+		bert_sent = ["/", "written", "/", "letters", "/", "112", "##c", "-", "l", "##01", "##4", ".", "tx", "##t", "june", "26", ",", "1995", "dear", "friend", ":"]
+		tracking = trackRawSentenceIndices(raw_sent, bert_sent)
+		print(tracking)
+		assert tracking == [0, 1, 2, 3, 4, 5, 5, 6, 7, 7, 7, 7, 7, 7, 8, 9, 10, 11, 12, 13, 14]
 
 	def test_raw_to_bert(self):
 		sent = ["I", "am", "gobbling"]
