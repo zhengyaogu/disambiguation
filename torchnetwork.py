73a3a383d71d43a0da0485d3e81beae385cc736d
zhengyaogu <zhengyao.gu30@gmail.com>
2019-07-31 15:21:17 -0700

    cudaify network

--- torchnetwork.py
+++ torchnetwork.py
@@ -5,7 +5,22 @@ from compare import sampleAllTrainingData, sampleTrainingDataFromFile
 from experiment import tensor_batcher
 from train import train_net
 from networks import SimpleClassifier
-trainingDataTensor = sampleAllTrainingData(100, 2000)
+trainingDataTensor = sampleAllTrainingData(100, 1000)
+
+if torch.cuda.is_available():
+    print("using gpu")
+    cuda = torch.device('cuda:0')
+    FloatTensor = torch.FloatTensor
+    LongTensor = torch.LongTensor
+    def cudaify(model):
+        return model.cuda()
+else: 
+    print("using cpu")
+    cuda = torch.device('cpu')
+    FloatTensor = torch.FloatTensor
+    LongTensor = torch.LongTensor
+    def cudaify(model):
+        return model
 
 
 numExamples = len(trainingDataTensor)
@@ -15,7 +30,7 @@ trainingData, testData = trainingDataTensor[:int(numExamples*.8)], trainingDataT
 
 trainLoader = tensor_batcher(trainingDataTensor, 1000)
 
-classifier = SimpleClassifier(1536, 96,2)
+classifier = cudaify(SimpleClassifier(1536, 96,2))
 train_net(classifier, trainingData, testData, tensor_batcher,
               batch_size=96, n_epochs=30, learning_rate=0.001,
               verbose=True)
