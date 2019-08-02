bf552d94a3ee57e775f28a0804c154e14f91a6e8
Kai <kai.pinckard@gmail.com>
2019-08-02 11:23:41 -0700

    fix queue lock error and add more processes

--- mp.py
+++ mp.py
@@ -1,45 +1,36 @@
-from multiprocessing import Process, Queue
+from multiprocessing import Pool, Queue, cpu_count, Manager
 from compare import createSentenceDictionaries
 import time
 import sys
 import json
 
 
-def writerToQueue(tuplee):
-    documents, formatted_data_queue = tuplee
-    for document in documents:
-        doc_dict = {}
-        doc_dict["docname"] = document["docname"]
-        doc_dict["doc"] = []
-        createSentenceDictionaries(document["doc"], doc_dict["doc"])
-        formatted_data_queue.put(doc_dict)
-        print("finished processing the document: " + document["docname"])
-
-
+def writerToQueue(document_queue_list):
+    document, formatted_data_queue = document_queue_list[0], document_queue_list[1]
+    doc_dict = {}
+    doc_dict["docname"] = document["docname"]
+    doc_dict["doc"] = []
+    createSentenceDictionaries(document["doc"], doc_dict["doc"])
+    formatted_data_queue.put(doc_dict)
+    print("finished processing the document: " + document["docname"])
 
 if __name__ == '__main__':
-    data_queue = Queue()
+   
     with open("googledata.json") as json_file:
         data = json.load(json_file)
 
-    small_chunk_size = len(data)//4
-    chunk1 = data[:small_chunk_size]
-    chunk2 = data[small_chunk_size:small_chunk_size*2]
-    chunk3 = data[small_chunk_size*2:small_chunk_size*3]
-    chunk4 = data[small_chunk_size*3:]
-    chunks = [chunk1, chunk2, chunk3, chunk4]
-
-    procs = []
-    for i, chunk in enumerate(chunks):
-        writer_p = Process(name="p_"+str(i),target=writerToQueue, args=((chunk, data_queue),))
-        writer_p.start()
-        procs.append(writer_p)
-    for proc in procs:
-        proc.join()
+    p = Pool(processes=cpu_count()*2)
+    m = Manager()
+    q = m.Queue()
+    bundle = []
+    for document in data:
+        bundle.append([document, q])
+    p.map(writerToQueue, bundle)
+    
 
     formatted_data = []
-    while not data_queue.empty():
-        formatted_data.append(data_queue.get())
+    while not q.empty():
+        formatted_data.append(q.get())
     with open("completedataV2.json", "w") as json_file:
         json.dump(formatted_data, json_file, indent=4)
     
